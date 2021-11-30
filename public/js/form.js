611121b70be178826f5a0066f15fde7e75025576
fenlon <1129507011@qq.com>
2021-11-30 11:19:12 +0800

    add js formcheck

--- public/js/form.js
+++ public/js/form.js
@@ -8,5 +8,72 @@ $(function () {
         }
         var newSrc = imgSrc + '?' + Math.random();
         img.attr('src', newSrc);
+    });
+
+    var formCheck = {
+        checkObj: null,
+        feedbackMark: $("#feedback-mark"),
+        check: function (object) {
+            this.checkObj = object;
+            var tagName = this.checkObj.prop("tagName").toLowerCase();
+            switch (tagName) {
+                case 'input':
+                    return this.inputCheck();
+                default:
+                    return false;
+            }
+        },
+        inputCheck: function(){
+            var val = this.checkObj.val();
+            return val ? this.validFeedback() : this.invalidFeedback();
+        },
+        invalidFeedback: function(){
+            this.checkObj.addClass("is-invalid").focus();
+            var placeholder = this.checkObj.prop("placeholder");
+            if (placeholder) {
+                this.feedbackMark.text("请输入"+placeholder).show();
+            }
+            return false;
+        },
+        validFeedback: function(){
+            this.checkObj.removeClass("is-invalid");
+            this.feedbackMark.hide();
+            return true;
+        },
+        formFeedback: function (msg) {
+            if (msg) {
+                this.feedbackMark.text(msg).show();
+            }
+        }
+    };
+
+    $("form").submit(function () {
+        var isValid = false;
+        $('[check-field="true"]').each(function () {
+            var object = $(this);
+            isValid = formCheck.check(object);
+            return isValid;
+        });
+        console.log(isValid);
+        if (isValid) {
+            var that = $(this);
+            $.post(that.attr("action"), that.serialize(), function (data) {
+                if (data.code == 200) {
+                    location.href = data.returnUrl;
+                } else {
+                    formCheck.formFeedback(data.msg);
+                    $('#get-captcha').click();
+                }
+            })
+        }
+        return false;
+    });
+
+    $(document).keydown(function (event) {
+        //alert(event.keyCode);
+        if (event.keyCode == 13) {
+            $("form").submit();
+            return false;
+        }
     })
 })
